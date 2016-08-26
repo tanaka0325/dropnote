532ef125d8cce003a6355ccae9f7dcaa11b187f3
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-08-26 19:09:44 +0900

    Implement Editor component

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import Codemirror from 'react-codemirror';
 
-export default function Editor() {
-  return (
-    <div className="pane">
-      <h1>hello dropnote</h1>
-    </div>
-  );
+import 'codemirror/mode/markdown/markdown';
+import 'codemirror/addon/edit/continuelist';
+
+export default class Editor extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      code: '',
+      mode: 'markdown',
+    };
+
+    this.updateCode = this.updateCode.bind(this);
+  }
+
+  updateCode(newCode) {
+    this.setState({
+      code: newCode,
+    });
+  }
+
+  render() {
+    const options = {
+      mode: this.state.mode,
+      lineNumbers: true,
+      extraKeys: {
+        Enter: 'newlineAndIndentContinueMarkdownList',
+      },
+    };
+
+    return (
+      <div className="pane">
+        <Codemirror value={this.state.code} onChange={this.updateCode} options={options} />
+      </div>
+    );
+  }
 }
