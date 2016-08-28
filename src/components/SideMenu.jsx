0e2ee7e8785c10173f492d00f8916ce520bd231f
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-08-28 22:42:48 +0900

    Change Sidemenu to class from pure function

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,31 +1,39 @@
 import React from 'react';
 
-export default function SideMenu() {
-  return (
-    <div className="pane-sm sidebar">
+export default class SideMenu extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
 
-      <nav className="nav-group">
-        <h5 className="nav-group-title">Recent Files</h5>
-        <span className="nav-group-item">
-          <span className="icon icon-doc-text" />
-          file
-        </span>
-        <span className="nav-group-item">
-          <span className="icon icon-doc-text" />
-          file
-        </span>
+  render() {
+    return (
+      <div className="pane-sm sidebar">
 
-        <h5 className="nav-group-title">Files</h5>
-        <a className="nav-group-item active">
-          <span className="icon icon-folder" />
-          directory
-        </a>
-        <span className="nav-group-item">
-          <span className="icon icon-doc-text" />
-          file
-        </span>
-      </nav>
+        <nav className="nav-group">
+          <h5 className="nav-group-title">Recent Files</h5>
+          <span className="nav-group-item">
+            <span className="icon icon-doc-text" />
+            file
+          </span>
+          <span className="nav-group-item">
+            <span className="icon icon-doc-text" />
+            file
+          </span>
 
-    </div>
-  );
+          <h5 className="nav-group-title">Files</h5>
+          <a className="nav-group-item active">
+            <span className="icon icon-folder" />
+            directory
+          </a>
+          <span className="nav-group-item">
+            <span className="icon icon-doc-text" />
+            file
+          </span>
+        </nav>
+
+      </div>
+    );
+  }
 }
